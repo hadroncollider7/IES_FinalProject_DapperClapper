1b6e55f502358b8fda5e407f834d88ec72131433
hadroncollider7 <chaulo74@students.rowan.edu>
2021-12-05 17:20:11 -0500

    write premliminary ADC ISR

--- Dapper_Clapper/main.c
+++ Dapper_Clapper/main.c
@@ -1,5 +1,7 @@
 #include <msp430.h> 
 
+/* Global variables */
+//volatile unsigned int preClap = 0; // make single clap work first
 
 /************************************************************************************
  * IES Final Project: Dapper Clapper
@@ -20,9 +22,9 @@ int main(void)
 	 * CONFIGURE I/O
 	 *********************************************************************************/
 	P1SEL &= ~(BIT7+BIT5+BIT4+BIT3+BIT2);   /* GPIO */
-	P6SEL |= BIT6;      /* ADC special function */
-	P2SEL |= BIT0;      /* TA1.1 special function */
-	P1DIR |= BIT7+BIT5+BIT4+BIT3+BIT2;  /* configure ports as output */
+	P6SEL |= BIT6;                          /* ADC special function */
+	P2SEL |= BIT0;                          /* TA1.1 special function */
+	P1DIR |= BIT7+BIT5+BIT4+BIT3+BIT2;      /* configure ports as output */
 
 	/*********************************************************************************
 	 * CONFIGURE ADC12
@@ -32,15 +34,19 @@ int main(void)
 	 * Conversion address: ADC12MEM0
 	 * Input channel: A6
 	 *********************************************************************************/
-	REFCTL0 &= ~REFMSTR;   /* Clear REFMSTR bit to hand control of ref to ADC12 */
-    ADC12CTL0 &= ~(ADC12ENC+ADC12SC);  /* clear to allow modification */
+	REFCTL0 &= ~REFMSTR;                /* Clear REFMSTR bit to hand control of ref to ADC12 */
+    ADC12CTL0 &= ~(ADC12ENC+ADC12SC);   /* clear to allow modification */
 	ADC12CTL0 |= ADC12ON + ADC12REF2_5V + ADC12SHT0_2;
-    ADC12CTL0 |= ADC12MSC;  /* first rising of SHI triggers repeating conversions */
+    ADC12CTL0 |= ADC12MSC;              /* first rising of SHI triggers repeating conversions */
     ADC12CTL1 |= ADC12CONSEQ_2 + ADC12SSEL_0 + ADC12SHP + ADC12SHS_0;
     ADC12MCTL0 |= ADC12INCH_6 + ADC12SREF_1;
     ADC12IE |= ADC12IE0;
     ADC12IFG &= ~ADC12IFG0;
 
+    volatile unsigned int j;
+    j = 0x30;
+    do j--;
+    while (j != 0);                     /* Delay for reference start-up */
 
     /*********************************************************************************
      * CONFIGURE TIMER TA1.1
@@ -49,18 +55,38 @@ int main(void)
      * TAR counts: 50
      * frequency_PWM: 20.9715 KHz
      * Output mode 7: reset/set
+     * Make sure to start the fan OFF
      *********************************************************************************/
-    TA1CTL |= TAIE + TACLR + TASSEL_2;
+    TA1CTL |= TAIE + TASSEL_2;
     TA1CCTL0 |= CCIE + OUTMOD_7;
     TA1CCTL0 &= ~CCIFG;
     TA1CCR0 = 0x31;     /* CCR0 = 49 */
-    TA1CTL |= MC_1;    /* start the timer */
+    TA1CCR1 = 0x00;     /* CCR1 = 0. The fan is off */
 
     /*********************************************************************************
-     * START THE ADC
+     * START THE ADC AND TIMER
      *********************************************************************************/
+    ADC12CTL0 |= ADC12ENC;  /* enable ADC12 conversion */
+    TA0CTL |= TACLR;        /* Clears TAR and divider logic (divider settings unchanged. Automatically resets */
+    TA1CTL |= MC_1;         /* start the timer */
 
-
+    __bis_SR_register(GIE); /* enable GIE to allow interrupts. */
+    __no_operation();         /* set breakpoint for debugging */
 
 	return 0;
+}
+
+/*********************************************************************************
+ * ADC12 INTERRUPT SERVICE ROUTINE
+ * Cutoff voltage: 80 mV, NADC = 0x83
+ *********************************************************************************/
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
+{
+    /* Make clapping work first, then implement preclap if there is time */
+    if (ADC12MEM0 >= 0x83)
+    {
+        P1OUT ^= BIT4;      /* toggle mode 1 LED */
+        TA1CCR1 = 44;
+    }
 }
