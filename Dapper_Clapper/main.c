a7cecad50e1c45346a6c18552eb5f487660dda72
hadroncollider7 <chaulo74@students.rowan.edu>
2021-12-05 16:07:46 -0500

    Configure I/O, ADC, and timer A1

--- Dapper_Clapper/main.c
+++ Dapper_Clapper/main.c
@@ -26,16 +26,34 @@ int main(void)
 
 	/*********************************************************************************
 	 * CONFIGURE ADC12
+	 * Voltage reference: 2.5 V - 0 v
+	 * Clock: ADC120SC (5 MHz MODCLK)
+	 * Conversion sequence: repeat single channel
+	 * Conversion address: ADC12MEM0
+	 * Input channel: A6
 	 *********************************************************************************/
 	REFCTLO0 &= ~REFMSTR;   /* Clear REFMSTR bit to hand control of ref to ADC12 */
     ADC12CTL0 &= ~(ADC12ENC+ADCSC)  /* clear to allow modification */
 	ADC12CTL0 |= ADC12ON + ADC12REF2_5V + ADC12SHT0_2;
     ADC12CTL0 |= ADC12MSC;  /* first rising of SHI triggers repeating conversions */
+    ADC12CTL1 |= ADC12CONSEQ_2 + ADC12SSEL_0 + ADC12SHP ADC12SHS_0;
+    ADC12MCTL0 |= ADC12INCH_6 + ADC12REF_1;
+    ADC12IE |= ADC12IE0;
+    ADC12IFG &= ~ADC12IFG0;
+
 
     /*********************************************************************************
      * CONFIGURE TIMER TA1.1
+     * Note: The PWM frequency needs to be at least 20 KHz to drive the motor.
+     * Clock: SMCKL (1.048576 MHz)
+     * TAR counts: 50
+     * frequency_PWM: 20.9715 KHz
+     * Output mode 7: reset/set
      *********************************************************************************/
-
+    TA1CTL |= TAIE + TACLR + TASSEL_2;
+    TA1CCTL0 |= CCIE + OUTMOD_7;
+    TA1CCTL0 &= ~CCIFG;
+    TA1CCR0 = 0x31;     /* CCR0 = 49 */
 
 
 	return 0;
