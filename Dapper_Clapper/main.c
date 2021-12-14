54db1864e937fc55faa6a3414d8d6d947c06a445
hadroncollider7 <chaulo74@students.rowan.edu>
2021-12-13 23:22:35 -0500

    Test ADC

--- Dapper_Clapper/main.c
+++ Dapper_Clapper/main.c
@@ -7,7 +7,7 @@
  * IES Final Project: Dapper Clapper
  * Version: 12-05-21
  * Inputs:
- *  - 3.3V (battery supply) -> AVCC
+ *  - 3V (battery supply) -> AVCC
  *  - GND (battery supply) -> AVSS
  *  - Mic analog -> P6.6 (A6)
  * Outputs:
@@ -28,7 +28,7 @@ int main(void)
 
 	/*********************************************************************************
 	 * CONFIGURE ADC12
-	 * Voltage reference: 2.5 V - 0 v
+	 * Voltage reference: 1.5 V - 0 v
 	 * Clock: ADC120SC (5 MHz MODCLK)
 	 * Conversion sequence: repeat single channel
 	 * Conversion address: ADC12MEM0
@@ -36,7 +36,7 @@ int main(void)
 	 *********************************************************************************/
 	REFCTL0 &= ~REFMSTR;                /* Clear REFMSTR bit to hand control of ref to ADC12 */
     ADC12CTL0 &= ~(ADC12ENC+ADC12SC);   /* clear to allow modification */
-	ADC12CTL0 |= ADC12ON + ADC12REF2_5V + ADC12SHT0_2;
+	ADC12CTL0 |= ADC12ON + ADC12REFON + ADC12SHT0_2;
     ADC12CTL0 |= ADC12MSC;              /* first rising of SHI triggers repeating conversions */
     ADC12CTL1 |= ADC12CONSEQ_2 + ADC12SSEL_0 + ADC12SHP + ADC12SHS_0;
     ADC12MCTL0 |= ADC12INCH_6 + ADC12SREF_1;
@@ -57,23 +57,26 @@ int main(void)
      * Output mode 7: reset/set
      * Make sure to start the fan OFF
      *********************************************************************************/
-    TA1CTL |= TAIE + TASSEL_2;
-    TA1CCTL0 |= CCIE + OUTMOD_7;
-    TA1CCTL0 &= ~CCIFG;
-    TA1CCR0 = 0x31;     /* CCR0 = 49 */
-    TA1CCR1 = 0x00;     /* CCR1 = 0. The fan is off */
+//    TA1CTL |= TAIE + TASSEL_2;
+//    TA1CCTL0 |= CCIE + OUTMOD_7;
+//    TA1CCTL0 &= ~CCIFG;
+//    TA1CCR0 = 0x31;     /* CCR0 = 49 */
+//    TA1CCR1 = 0x00;     /* CCR1 = 0. The fan is off */
 
     /*********************************************************************************
      * START THE ADC AND TIMER
      *********************************************************************************/
     ADC12CTL0 |= ADC12ENC;  /* enable ADC12 conversion */
-    TA0CTL |= TACLR;        /* Clears TAR and divider logic (divider settings unchanged. Automatically resets */
-    TA1CTL |= MC_1;         /* start the timer */
+//    TA0CTL |= TACLR;        /* Clears TAR and divider logic (divider settings unchanged. Automatically resets */
+//    TA1CTL |= MC_1;         /* start the timer */
+
+    P1OUT &= ~BIT4;
+    ADC12CTL0 |= ADC12SC;
 
     __bis_SR_register(GIE); /* enable GIE to allow interrupts. */
     __no_operation();         /* set breakpoint for debugging */
 
-	return 0;
+    while(1);
 }
 
 /*********************************************************************************
@@ -84,9 +87,17 @@ int main(void)
 __interrupt void ADC12_ISR(void)
 {
     /* Make clapping work first, then implement preclap if there is time */
-    if (ADC12MEM0 >= 0x83)
+    if (ADC12MEM0 >= 0x0AAA)
     {
         P1OUT ^= BIT4;      /* toggle mode 1 LED */
-        TA1CCR1 = 44;
+        //        TA1CCR1 = 44;
+        volatile unsigned int j;
+        j = 0xFFFF;
+        do j--;
+        while (j != 0);
+    }
+    else
+    {
+        ADC12IFG &= ~ADC12IFG0;
     }
 }
